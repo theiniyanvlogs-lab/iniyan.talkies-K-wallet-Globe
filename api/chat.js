96baa824105cc45c70d5c1afd72c259b1e10d99d
theiniyanvlogs-lab <the.iniyan.vlogs@gmail.com>
2026-02-05 21:40:33 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
           model: "llama-3.1-8b-instant",
 
           // ✅ Very high token limit for full answers
-          max_tokens: 5000,
+          max_tokens: 1000,
           temperature: 0.6,
 
           messages: [
